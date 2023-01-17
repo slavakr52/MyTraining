bc4b5ec538c9f0ddabbc4c777218d3305c1a23ef
slavakr52 <slavakr52@mail.ru>
2023-01-17 11:27:19 +0300

    добавил быструю сортировку

--- Tasks003_Sorting/Sorting.cs
+++ Tasks003_Sorting/Sorting.cs
@@ -43,6 +43,32 @@ public static class Sorting
         }
         return array;
     }
+
+    public static int[] SortQuick(int[] collection, int left, int right)
+    {
+        int i = left;
+        int j = right;
+
+        int pivot = collection[Random.Shared.Next(left, right)];
+        while (i <= j)
+        {
+            while (collection[i] < pivot) i++;
+            while (collection[j] > pivot) j--;
+
+            if (i <= j)
+            {
+                int t = collection[i];
+                collection[i] = collection[j];
+                collection[j] = t;
+                i++;
+                j--;
+            }
+        }
+        if (i < right) SortQuick(collection, i, right);
+        if (left < j) SortQuick(collection, left, j);
+        return collection;
+    }
 }
+
 
 
--- Tasks003_Sorting/Start.cs
+++ Tasks003_Sorting/Start.cs
@@ -10,11 +10,13 @@ Console.WriteLine();
 Console.WriteLine("Выберите вид сортировки:");
 Console.WriteLine("1. Сортировка выбором");
 Console.WriteLine("2. Пузырьковая сортировка");
+Console.WriteLine("3. Быстрая сортировка");
 int sortNum = int.Parse(Console.ReadLine()!);
 
-if (sortNum != 1 && sortNum != 2) Console.WriteLine("Такой сортировки не указано");
+if (sortNum != 1 && sortNum != 2 && sortNum != 3) Console.WriteLine("Такой сортировки не указано");
 else
 {
     if (sortNum == 1) ChooseSortArray(array);
     if (sortNum == 2) BubbleSortArray(array);
+    if (sortNum == 3) {SortQuick(array, 0, array.Length - 1); Console.WriteLine($"Отсортированный массив: [{String.Join(", ", array)}]");}
 }
